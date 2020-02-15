94aa456566c454c6e67961a5c83d5e9cd5c44fa2
PawelRod <rodziewiczpp@gmail.com>
2020-02-15 23:36:04 +0100

    Add html structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 import './scss/style.scss';
 
-function component() {
-    const element = document.createElement('div');
+// function component() {
+//     const element = document.createElement('div');
   
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
-    return element;
-  }
+//     return element;
+//   }
   
-  document.body.appendChild(component());
+//   document.body.appendChild(component());
 
 
