3418087f73334a30ea86da55e5e8343ccfae4c92
PawelRod <rodziewiczpp@gmail.com>
2020-02-16 17:01:55 +0100

    Add simple passwordGen function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './scss/style.scss'
+
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var char = "";
+
+function passwordGen() {
+    for(let i = 0; i < 10; i++) {
+        char += letters.charAt(Math.floor(Math.random() * letters.length));
+    }
+    return char;
+};
+
+console.log(passwordGen())
